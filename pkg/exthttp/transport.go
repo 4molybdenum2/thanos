93e15b540871a612f35f2cc54b62f1d677ef8cb0
4molybdenum2 <tathagatapaul7@gmail.com>
2021-09-11 15:23:58 +0530

    Added a DefaultTransport function in package exhttp

--- pkg/exthttp/transport.go
+++ pkg/exthttp/transport.go
@@ -4,10 +4,29 @@
 package exthttp
 
 import (
+	"crypto/tls"
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/common/model"
 )
+
+// HTTPConfig stores the http.Transport configuration for the s3 minio client.
+type HTTPConfig struct {
+	IdleConnTimeout       model.Duration `yaml:"idle_conn_timeout"`
+	ResponseHeaderTimeout model.Duration `yaml:"response_header_timeout"`
+	InsecureSkipVerify    bool           `yaml:"insecure_skip_verify"`
+
+	TLSHandshakeTimeout   model.Duration `yaml:"tls_handshake_timeout"`
+	ExpectContinueTimeout model.Duration `yaml:"expect_continue_timeout"`
+	MaxIdleConns          int            `yaml:"max_idle_conns"`
+	MaxIdleConnsPerHost   int            `yaml:"max_idle_conns_per_host"`
+	MaxConnsPerHost       int            `yaml:"max_conns_per_host"`
+	DisableCompression    bool	     `yaml:"disable_compression"`
+	// Allow upstream callers to inject a round tripper
+	Transport http.RoundTripper `yaml:"-"`
+}
 
 // NewTransport creates a new http.Transport with default settings.
 func NewTransport() *http.Transport {
@@ -23,5 +42,27 @@ func NewTransport() *http.Transport {
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
+
+// Default Transport function that creates a http.Transport with default settings.
+func DefaultTransport(config HTTPConfig) *http.Transport {
+	return &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+
+		MaxIdleConns:          config.MaxIdleConns,
+		MaxIdleConnsPerHost:   config.MaxIdleConnsPerHost,
+		IdleConnTimeout:       time.Duration(config.IdleConnTimeout),
+		MaxConnsPerHost:       config.MaxConnsPerHost,
+		TLSHandshakeTimeout:   time.Duration(config.TLSHandshakeTimeout),
+		ExpectContinueTimeout: time.Duration(config.ExpectContinueTimeout),
+		ResponseHeaderTimeout: time.Duration(config.ResponseHeaderTimeout),
+		DisableCompression:	config.DisableCompression,
+		TLSClientConfig:	&tls.Config{InsecureSkipVerify: config.InsecureSkipVerify},
 	}
 }
